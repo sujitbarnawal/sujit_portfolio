8e8a12c7e272621e6584d5e36a118aa061104ea0
sujitbarnawal <sujitbarnawal4545@gmail.com>
2025-05-17 16:34:43 +0545

    resume added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href=""
+            href="https://drive.google.com/file/d/1nhpEyf6lferDXnqYMvTwBAtkGM8uHT9U/view"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
